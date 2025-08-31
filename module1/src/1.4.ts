46a52fcf335b5194d67456ce31fb77e4e334b8cf
Arzenaakter <arzenaakter@gmail.com>
2025-08-31 14:22:09 +0600

    basic data type

---
+++ module1/src/1.4.ts
@@ -0,0 +1,24 @@
+// Basic data type
+
+// string
+let firstName: string = "Arzena";
+
+// number
+let rollNo: number = 22;
+
+// boolean
+let isAdmin: boolean = true;
+
+// undefined
+let x: undefined = undefined;
+
+// null
+let y: null = null;
+
+// array
+let fruites: string[] = ["Apple", "Banana", "Orange"];
+
+let rolls: number[] = [1, 4, 5];
+
+// touple --> array with different data types
+let person: [string, number, boolean] = ["arzena", 25, true];
